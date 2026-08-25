76cd475ad22a66ec4f6b062eaf1736702546e829
Seif <seifkaram991@gmail.com>
2026-08-26 00:39:24 +0300

    clean the Repo

--- eCommerce.Core/obj/Debug/net10.0/eCommerce.Core.AssemblyInfo.cs
+++ eCommerce.Core/obj/Debug/net10.0/eCommerce.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eCommerce.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cd1e75e39542049078ed5e63b10de247c702c6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14999501c25777b667ad950e11dfefa9da76f12d")]
 [assembly: System.Reflection.AssemblyProductAttribute("eCommerce.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eCommerce.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
